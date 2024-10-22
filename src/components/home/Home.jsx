03c7cd3ab8c9a3d4045d54251594e8fd44f00db2
Declan <144496393+dec-gr@users.noreply.github.com>
2024-10-22 21:40:48 +0100

    Format home screen

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,21 @@
 import styles from './Home.module.css';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div className={styles.homeCont}>
-      <h1>Welcome to the shop</h1>
+      <div className={styles.homeLeftDiv}>
+        <h1>Apparael Co.</h1>
+        <h2>Fashion for Every Day.</h2>
+        <button>
+          <h2>
+            <Link to="shop">Shop Now</Link>
+          </h2>
+        </button>
+      </div>
+      <div className={styles.homeRightDiv}>
+        <img src="src/assets/fleeceRack.jpg" alt="" />
+      </div>
     </div>
   );
 };
